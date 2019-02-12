a093ef71ce9ee862219312d9e70081e62f064a8b
rickligang <rickligang@didichuxing.com>
2019-02-12 14:31:03 +0800

    init

---
+++ src/test/java/com/lg/netty/EchoServer.java
@@ -0,0 +1,45 @@
+package com.lg.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import java.net.InetSocketAddress;
+import org.junit.Test;
+
+/**
+ * @author ligang
+ * @date 2019-02-11
+ */
+public class EchoServer {
+
+    private final int port = 8001;
+
+    @Test
+    public void startNetty() throws Exception {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                .channel(NioServerSocketChannel.class)
+                .localAddress(new InetSocketAddress(port))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline()
+                            .addLast(serverHandler);
+                    }
+                });
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
+
+}
